0ae60d7ad65708c890b25d94afa3a06be6613960
nickgallimore <nfgallimore@gmail.com>
2015-08-24 05:12:32 -0400

    Added routes based on user ID.

--- routes/runs.js
+++ routes/runs.js
@@ -4,11 +4,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/users/:id?', function(req, res, next) {
+router.get('/', function(req, res, next) {
     var Parse = require('parse').Parse;
     Parse.initialize("sD4tDFzNyuas8Vg0VhoXeF5OSnLHMkJRLxuHOkUL", "ntKIRdfzedSkLFGaj99qrC2lG2VNOXdWIrONcVIP");
-    var id = req.params.id;
+    var Run = Parse.Object.extend("Run");
+    var query = new Parse.Query(Run);
+    query.find({
+        success: function (results) {
+            // Do something with the returned Parse.Object values
+            for (var i = 0; i < results.length; i++) {
+                var object = results[i];
+                console.log(object.get('time'));
+            }
+            res.send(results);
+        },
+        error: function (error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+});
+
+/* GET users listing. */
+router.get('/:user', function(req, res, next) {
+    var Parse = require('parse').Parse;
+    Parse.initialize("sD4tDFzNyuas8Vg0VhoXeF5OSnLHMkJRLxuHOkUL", "ntKIRdfzedSkLFGaj99qrC2lG2VNOXdWIrONcVIP");
+    var id = req.params.user;
     if (id) {
         var Run = Parse.Object.extend("Run");
         var query = new Parse.Query(Run);
@@ -28,6 +48,21 @@ router.get('/users/:id?', function(req, res, next) {
         });
     }
     else {
+        var Run = Parse.Object.extend("Run");
+        var query = new Parse.Query(Run);
+        query.find({
+            success: function (results) {
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    console.log(object.get('time'));
+                }
+                res.send(results);
+            },
+            error: function (error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
         next();
     }
 });
