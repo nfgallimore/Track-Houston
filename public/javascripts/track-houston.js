ed4979fd85b696f2049fa7564d010b8dd0361cb2
nickgallimore <nfgallimore@gmail.com>
2015-10-09 13:15:43 -0500

    endless loop

--- public/javascripts/track-houston.js
+++ public/javascripts/track-houston.js
@@ -3,7 +3,7 @@
  */
 
 angular.module('track-houston', []).run(['$rootScope', function ($scope) {
-    Parse.initialize("sD4tDFzNyuas8Vg0VhoXeF5OSnLHMkJRLxuHOkUL", "ntKIRdfzedSkLFGaj99qrC2lG2VNOXdWIrONcVIP");
+    Parse.initialize("sD4tDFzNyuas8Vg0VhoXeF5OSnLHMkJRLxuHOkUL", "ntKIRdfzedSkLFGaj99qrC2lG2VNOXdWIrONcVIP", "");
     $scope.scenario = 'Log in';
     $scope.currentUser = Parse.User.current();
     $scope.createCoachSuccess = null;
@@ -12,19 +12,16 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
     $scope.isAdmin = false;
     $scope.isCoach = false;
     $scope.isStudent = false;
-    $scope.runs = [{}];
-    $scope.noitems = [];
-    $scope.runsJSON = [];
     $scope.createAdminSuccess = null;
     $scope.createAdmin = function () {
-        Parse.User.enableRevocableSession();
-        var currUserToken = $scope.currentUser.getSessionToken();
         var admin = new Parse.User;
-        admin.set("username", ($("#adminForm-fname").val().charAt(0) + $("#adminForm-lname").val()).toLowerCase());
+        var fname = $('#adminForm-fname').val().toLowerCase();
+        var lname = $("#adminForm-lname").val().toLowerCase();
+        admin.set("username", (fname.charAt(0) + lname));
         admin.set("password", "password");
         admin.set("type", "admin");
-        admin.set("fname", $("#adminForm-fname").val().toLowerCase());
-        admin.set("lname", $("#adminForm-lname").val().toLowerCase());
+        admin.set("fname", fname);
+        admin.set("lname", lname);
         var email = $("#adminForm-email").val();
         if (email.indexOf("@") > -1 || email.indexOf("." > -1)) {
             $scope.createAdminSuccess = 'Invalid Email'
@@ -35,12 +32,6 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                 admin.save();
                 $scope.createAdminSuccess = 'Coach successfully created!';
                 $scope.$apply();
-                Parse.User.become(currUserToken, null).then(function (newUser) {
-                    // The current user is now set to user.
-                }, function (error) {
-                    // The token could not be validated.
-                });
-                console.log("Good news boss, I Successfully ran the function createCoach()!");
             },
             error: function (coach, error) {
                 alert("Unable to sign up:  " + error.code + " " + error.message);
@@ -79,7 +70,7 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
             }
         });
     };
-    $scope.logOut = function (form) {
+    $scope.logOut = function () {
         Parse.User.logOut();
         $scope.currentUser = null;
         $scope.isAdmin = false;
@@ -89,18 +80,33 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
     $scope.submitRunForm = function () {
         var Run = Parse.Object.extend("Run");
         var run = new Run();
-        run.set("name", $("#name").val());
+        var name = $("#name").val();
+        var fname = "";
+        var lname = "";
+        for (var i = 0; i < name.length; i++) {
+            if (name[i] === ' ') {
+                fname = name.slice(0, i);
+                lname = name.slice(i + 1, name.length);
+            }
+        }
+        //console.log(fname);
+        //console.log(lname);
+
+        run.set("name", name);
         run.set("time", $("#time").val());
         run.set("event", $("#event").val());
         run.set("date", $("#datepicker1").val());
+        run.set("fname", fname);
+        run.set("lname", lname);
         var username = "";
         var User = Parse.Object.extend("_User");
         var query = new Parse.Query(User);
         query.find({
             success: function (results) {
                 for (var i = 0; i < results.length; i++) {
-                    if ((results[i].get("fname") + " " + results[i].get("lname")).equals($("#name").val())) {
-                        username = results[i].get("username");
+                    //console.log(results[i].get("name"))
+                    if (results[i].get("fname") + " " + results[i].get("lname") === $("#name").val()) {
+                        username = results[i].get("username").toLowerCase();
                     }
                 }
             },
@@ -108,19 +114,47 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                 alert("Unable to log in: " + error.code + " " + error.message);
             }
         });
+        username = $scope.lookUpUsernameByName(name);
         run.set("username", username);
         run.save(null, {
             success: function (run) {
                 run.save();
-                console.log("Successfully ran the function submitRunForm()!");
+                //console.log("Successfully ran the function submitRunForm()!");
             }
         })
     };
+    //$scope.createDiv = function (id, html) {
+    //    var div = document.createElement("div");
+    //    document.getElementById("tabs").appendChild(div);
+    //    div.id = id;
+    //    div.innerHTML = html;
+    //};
+    //$scope.renderProfile = function (name) {
+    //    $scope.hideDiv("runTable");
+    //    var html = name + "\'s run times:";
+    //    $scope.createDiv("profile", html);
+    //};
+    //$scope.home = function() {
+    //    $scope.hideDiv("profile");
+    //    $scope.showDiv("runTable");
+    //};
+    //$scope.hideForms = function() {
+    //    document.getElementById("rundata").style.display = 'none';
+    //    document.getElementById("coachCreateAccount").style.display = 'none';
+    //    document.getElementById("adminCreateAccount").style.display = 'none';
+    //    document.getElementById("runTable").style.display = 'none';
+    //};
+    //$scope.showForms = function() {
+    //    document.getElementById("rundata").style.display = 'block';
+    //    document.getElementById("coachCreateAccount").style.display = 'block';
+    //    document.getElementById("adminCreateAccount").style.display = 'block';
+    //    document.getElementById("runTable").style.display = 'block';
+    //};
     $scope.submitGradeForm = function () {
         var Grade = Parse.Object.extend("Grade");
         var grade = new Grade();
         grade.set("name", $scope.currentUser.get("username"));
-        console.log($scope.currentUser.get("username"));
+        //console.log($scope.currentUser.get("username"));
         grade.set("grade1", $("#class1").val() + ": " + $("#grade1").val());
         grade.set("grade2", $("#class2").val() + ": " + $("#grade2").val());
         grade.set("grade3", $("#class3").val() + ": " + $("#grade3").val());
@@ -130,18 +164,29 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
         grade.save(null, {
             success: function (grade) {
                 grade.save();
-                console.log("Successfully ran the function submitGradeForm()!");
+                //console.log("Successfully ran the function submitGradeForm()!");
             }
         })
     };
+    //$scope.hideDiv = function(id) {
+    //    (function($) {
+    //        $('#' + id).hide();
+    //    })(jQuery);
+    //};
+    //$scope.showDiv = function(id) {
+    //    (function($) {
+    //        $('#' + id).show();
+    //    })(jQuery);
+    //};
     $scope.createStudent = function () {
-        Parse.User.enableRevocableSession();
         var currUserToken = $scope.currentUser.getSessionToken();
         var student = new Parse.User;
-        student.set("username", ($("#studentForm-fname").val().charAt(0) + $("#studentForm-lname").val()).toLowerCase());
+        var fname = $("#studentForm-fname").val();
+        var lname = $("#studentForm-lname").val().toLowerCase();
+        student.set("username", (fname.charAt(0) + lname));
         student.set("password", "password");
-        student.set("fname", $("#studentForm-fname").val().toLowerCase());
-        student.set("lname", $("#studentForm-lname").val().toLowerCase());
+        student.set("fname", fname);
+        student.set("lname", lname);
         student.set("pname", $("#studentForm-parentName").val().toLowerCase());
         student.set("byear", $("#studentForm-birthYear").val().toLowerCase());
         student.set("psite", $("#studentForm-practiceSite").val().toLowerCase());
@@ -156,7 +201,7 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                 }, function (error) {
                     // The token could not be validated.
                 });
-                console.log("Good news boss, I Successfully ran the function createStudent()!");
+                //console.log("Good news boss, I Successfully ran the function createStudent()!");
             },
             error: function (student, error) {
                 alert("Unable to sign up:  " + error.code + " " + error.message);
@@ -189,7 +234,7 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                 }, function (error) {
                     // The token could not be validated.
                 });
-                console.log("Good news boss, I Successfully ran the function createCoach()!");
+                //console.log("Good news boss, I Successfully ran the function createCoach()!");
             },
             error: function (coach, error) {
                 alert("Unable to sign up:  " + error.code + " " + error.message);
@@ -207,7 +252,7 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                         var fname = results[i].get("fname");
                         var lname = results[i].get("lname");
                         var name = fname + " " + lname;
-                        console.log(name);
+                        //console.log(name);
                         return name;
                     }
                 }
@@ -227,7 +272,7 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                     var lname = results[i].get("lname");
                     var n = fname + " " + lname;
                     if (n === name) {
-                        console.log(results[i].get("username"));
+                        //console.log(results[i].get("username"));
                         return results[i].get("username");
                     }
                 }
@@ -249,7 +294,7 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                     var name = fname + " " + lname;
                     var username = $scope.lookUpUsernameByName(name);
                     namesArr.push(name);
-                    console.log(name);
+                    //console.log(name);
                     (function ($) {
                         $('#name').append($('<option>', {
                             value: username,
@@ -262,7 +307,7 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
                 alert("Unable to sign up:  " + error.code + " " + error.message);
             }
         });
-        console.log(namesArr);
+        //console.log(namesArr);
         for (var i = 0; i < namesArr.length; i++) {
             $('#name').append($('<option>', {
                 value: $scope.lookUpUsernameByName(namesArr[i]),
@@ -277,18 +322,21 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
             success: function (results) {
                 for (var i = 0; i < results.length; i++) {
                     var object = results[i];
-                    (function ($) {
-                        $('#rundata').append('<tr><td><a href=#' + object.get('username') + '>' + object.get('name') + '</a></td><td>' + object.get('time') + '</td><td>' + object.get('event') + '</td><td>' + object.get('date') + '</td></tr>');
-                    })(jQuery);
                     var name = object.get('name');
+                    (function ($) {
+                        var append = "<tr><td><a href=";
+                        append += "\"javascript:renderProfile(\'";
+                        append += name;
+                        append += "\')\">";
+                        append += object.get('name') + '</a></td><td>' + object.get('time') + '</td><td>' + object.get('event') + '</td><td>' + object.get('date') + '</td></tr>';
+                        $('#rundata').append(append);
+                    })(jQuery);
                     var time = object.get('time');
-                    var event = object.get('event');
+                    var event;
+                    event = object.get('event');
                     var date = object.get('date');
                     var obj = {Name: name, Time: time, Event: event, Date: date};
-                    $scope.runs.push(obj);
-                    $scope.runsJSON.push(object.toJSON())
                 }
-                console.log($scope.runsJSON);
             },
             error: function (error) {
                 alert("Error: " + error.code + " " + error.message);
@@ -337,10 +385,17 @@ angular.module('track-houston', []).run(['$rootScope', function ($scope) {
             $scope.newAcctType = "admin";
         }
     };
-    $scope.newAcctTypeUpdate();
-    $scope.createRunTable();
-    $scope.createGradeTable();
-    $scope.populateDropdown();
-    $scope.lookUpUsernameByName("john johnson");
-    $scope.lookUpNameByUsername("jjohnson");
+    // initial functions to call
+    $scope.init = function () {
+        $scope.newAcctTypeUpdate();
+        $scope.createRunTable();
+        $scope.createGradeTable();
+        $scope.populateDropdown();
+    };
+    //$scope.init();
+    //var elements = document.getElementsByClassName("homeButton");
+
+    //for (var e in elements) {
+    //    e.prop("href", "javascript:home()");
+    //}
 }]);
