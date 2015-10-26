408dc862bad8bad0aba1a29ec9224b83e27957ef
nickgallimore <nfgallimore@gmail.com>
2015-10-26 04:47:26 -0400

    Deploy

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,55 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', {title: 'Track-Houston'});
 });
-
+/* GET users listing. */
+router.get('/:user', function (req, res, next)
+{
+    var Parse = require('parse').Parse;
+    Parse.initialize("sD4tDFzNyuas8Vg0VhoXeF5OSnLHMkJRLxuHOkUL", "ntKIRdfzedSkLFGaj99qrC2lG2VNOXdWIrONcVIP");
+    var id = req.params.user;
+    if (id)
+    {
+        var User = Parse.Object.extend("_User");
+        var query = new Parse.Query(User);
+        query.equalTo("username", id);
+        query.find({
+            success: function (results)
+            {
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++)
+                {
+                    var object = results[i];
+                    console.log(object.get('time'));
+                }
+                res.render('profile', results);
+            },
+            error: function (error)
+            {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    }
+    else
+    {
+        var UserNew = Parse.Object.extend("_User");
+        var queryNew = new Parse.Query(UserNew);
+        queryNew.find({
+            success: function (results)
+            {
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++)
+                {
+                    var object = results[i];
+                    console.log(object.get('time'));
+                }
+                res.send(results);
+            },
+            error: function (error)
+            {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+        next();
+    }
+});
 module.exports = router;
